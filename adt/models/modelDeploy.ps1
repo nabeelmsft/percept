e9c889d96fe8d479d46096ba906ec56b25f0f586
Muhammad Nabeel <munabeel@microsoft.com>
2021-05-27 11:17:49 -0700

    Added site floor model and update model deployment

--- adt/models/modelDeploy.ps1
+++ adt/models/modelDeploy.ps1
@@ -1,4 +1,24 @@
 $dtname = "santacruz3203-digitaltwin"
-$sitemodelid = $(az dt model create -n $dtname --models .\SiteInterface.json --query [].id -o tsv)
-az dt twin create -n $dtname --dtmi $sitemodelid --twin-id "PerceptSite"
 
+$sitemodelid = "dtmi:percept:DigitalTwins:Site;1"
+echo "Deleting Model:" 
+echo $sitemodelid
+az dt model delete --dt-name $dtname --dtmi $sitemodelid
+$sitemodelid = $(az dt model create -n $dtname --models .\SiteInterface.json --query [].id -o tsv)
+
+$sitefloormodelid = "dtmi:percept:DigitalTwins:SiteFloor;1"
+echo "Deleting Model:"
+echo  $sitefloormodelid
+az dt model delete --dt-name $dtname --dtmi $sitefloormodelid
+$sitefloormodelid = $(az dt model create -n $dtname --models .\SiteFloorInterface.json --query [].id -o tsv)
+
+az dt twin delete-all --dt-name $dtname
+echo "Creating twin: PerceptSite"
+az dt twin create -n $dtname --dtmi $sitemodelid --twin-id "PerceptSite"
+echo "Creating twin: PerceptSiteFloor"
+az dt twin create -n $dtname --dtmi $sitefloormodelid --twin-id "PerceptSiteFloor"
+
+$relname = "rel_has_floors"
+echo "Creating relationships"
+echo $relname
+az dt twin relationship create -n $dtname --relationship $relname --twin-id "PerceptSite" --target "PerceptSiteFloor" --relationship-id "Site has floors"
