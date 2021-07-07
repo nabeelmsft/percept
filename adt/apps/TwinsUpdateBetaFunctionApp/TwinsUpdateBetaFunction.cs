4658e202bf60123cc2e3522c93d7d32fa9b1d9cd
Muhammad Nabeel <munabeel@microsoft.com>
2021-07-07 16:13:47 -0700

    Fixed pascal casing of referenced namespace.

--- adt/apps/TwinsUpdateBetaFunctionApp/TwinsUpdateBetaFunction.cs
+++ adt/apps/TwinsUpdateBetaFunctionApp/TwinsUpdateBetaFunction.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TwinsUpdateBetaFunctionApp.model;
+using TwinsUpdateBetaFunctionApp.Model;
 
 namespace TwinsUpdateBetaFunctionApp
 {
