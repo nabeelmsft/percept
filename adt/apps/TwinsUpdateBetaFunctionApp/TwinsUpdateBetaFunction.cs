ad696a50db1625113fc45d8dbfbcd993b2a0dce2
Muhammad Nabeel <munabeel@microsoft.com>
2021-07-08 00:21:15 -0700

    Updated code changes to simplify the deserialization.

--- adt/apps/TwinsUpdateBetaFunctionApp/TwinsUpdateBetaFunction.cs
+++ adt/apps/TwinsUpdateBetaFunctionApp/TwinsUpdateBetaFunction.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using TwinsUpdateBetaFunctionApp.Model;
 
 namespace TwinsUpdateBetaFunctionApp
@@ -12,52 +11,21 @@ namespace TwinsUpdateBetaFunctionApp
     public static class TwinsUpdateBetaFunction
     {
         [FunctionName("TwinsUpdateBetaFunction")]
-        public static async Task Run([EventHubTrigger("santacruz3203-digitaltwin-eventhub", Connection = "EventHubConnection")] EventData[] events, ILogger log)
+        public static void Run([EventHubTrigger("santacruz3203-digitaltwin-eventhub", Connection = "EventHubConnection")] EventData[] events, ILogger log)
         {
             var exceptions = new List<Exception>();
-            List<TwinUpdate> twinUpdates = new List<TwinUpdate>();
-
             foreach (EventData eventData in events)
             {
                 try
                 {
-                    TwinMessage twinMessage = eventData.EventBody.ToObjectFromJson<TwinMessage>();
-                    if (twinMessage.patch != null)
-                    {
-                        TwinUpdate twinUpdate = new TwinUpdate();
-                        twinUpdate.ModelId = twinMessage.modelId;
-                        foreach (Patch patchItem in twinMessage.patch)
-                        {
-                            if(patchItem.path.Equals("/FloorId", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                twinUpdate.Floor = patchItem.value;
-                            }
-
-                            if (patchItem.path.Equals("/FloorName", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                twinUpdate.FloorName = patchItem.value;
-                            }
-
-                            if (patchItem.path.Equals("/Label", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                twinUpdate.Label = patchItem.value;
-                            }
-
-                            if (patchItem.path.Equals("/Confidence", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                twinUpdate.Confidence = patchItem.value;
-                            }
-
-                            if (patchItem.path.Equals("/timestamp", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                twinUpdate.Timestamp = patchItem.value;
-                            }
-                        }
-
-                        twinUpdates.Add(twinUpdate);
-                    }
                     log.LogInformation($"Message received: {eventData.EventBody}");
-                    await Task.Yield();
+
+                    // Deserialize the message into TwinMessage object. TwinMessage is a custom class representing the schema of Azure Digital Twins message.
+                    TwinMessage twinMessage = eventData.EventBody.ToObjectFromJson<TwinMessage>();
+
+                    // Here add your code to further process TwinMessage.
+
+                    log.LogInformation($"Message processed");
                 }
                 catch (Exception e)
                 {
